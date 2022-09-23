c7da871e3096cc593a2b6467cf64dec18efe3dd7
tanishtha jawa <tanishtha.jawa@thoughtworks.com>
2022-09-23 18:56:04 +0530

    Add. use of props in class components.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Greet from "./components/Greet"
+import Welcome from "./components/Welcome";
 
 function App() {
 
@@ -11,6 +12,13 @@ function App() {
         <button>Action</button>
       </Greet>
       <Greet name="Draco" age="30" />
+      <Welcome name="Alice" age="10">
+        <p> This is child props</p>
+      </Welcome>
+      <Welcome name="Diana" age="20" >
+        <button>Action</button>
+      </Welcome>
+      <Welcome name="Draco" age="30" />
     </div>
 
   );
---
+++ src/components/Welcome.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+class Welcome extends Component {
+    render() {
+        return (
+            <div>
+                <h1>
+                    Welcome {this.props.name}<br/>
+                    Age {this.props.age}
+
+                </h1>
+                { this.props.children }
+            </div>
+           
+        )
+    }
+}
+export default Welcome
