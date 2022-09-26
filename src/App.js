01b039e07755d5f657d62f79f7cdca51484f8579
tanishtha jawa <tanishtha.jawa@thoughtworks.com>
2022-09-26 10:40:14 +0530

    Add. setState use

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import Greet from "./components/Greet"
 import Welcome from "./components/Welcome";
-
+import Message from "./components/Message";
+import Counter from "./Counter";
 function App() {
 
   return (
     <div className="App">
-      <Greet name="Alice" age="10">
+      {/* <Greet name="Alice" age="10">
         <p> This is child props</p>
       </Greet>
       <Greet name="Diana" age="20" >
         <button>Action</button>
       </Greet>
-      <Greet name="Draco" age="30" />
       <Welcome name="Alice" age="10">
         <p> This is child props</p>
       </Welcome>
       <Welcome name="Diana" age="20" >
         <button>Action</button>
       </Welcome>
-      <Welcome name="Draco" age="30" />
+      <Message /> */}
+      <Counter />
     </div>
 
   );
