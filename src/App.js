a9a9dfd96581b1c2729bfc37ef3eb891cb8163e9
tanishtha jawa <tanishtha.jawa@thoughtworks.com>
2022-09-28 13:19:25 +0530

    Refs with class component

--- src/App.js
+++ src/App.js
@@ -18,13 +18,15 @@ import Table from "./components/Table";
 import PureComp from "./components/PureComp";
 import ParentComp from "./components/ParentComp";
 import RefsDemo from "./components/RefsDemo";
+import FocusInput from "./components/FocusInput";
 
 
 function App() {
 
   return (
     <div className="App">
-      <RefsDemo />
+      <FocusInput />
+      {/* <RefsDemo /> */}
       {/* <ParentComp /> */}
       {/* <FragmentDemo />
       <Table />
